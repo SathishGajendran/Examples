927d86bbfb34fd1eb159bfe5b88b88b6679186b7
Sathish G <sathish.gajendran@gmail.com>
2015-09-30 11:03:10 +0530

    openshift basic template changes

--- OpenShiftHelloWorld/app.js
+++ OpenShiftHelloWorld/app.js
@@ -1,76 +1,43 @@
-/**
- * Created by Sathish.G on 8/19/15.
- */
+// Application Main file
 
-var express = require('express');
-var fs      = require('fs');
-var http=require('http');
-var bodyParser=require('body-parser');
-var path=require('path');
+var express = require('express'),
+	bodyParser = require('body-parser'),
+	http = require('http'),
+	app = express();
 
-var SampleApp = function() {
-    var self = this;
-    self.setupVariables = function() {
-        self.ipaddress = process.env.OPENSHIFT_NODEJS_IP;
-        self.port      = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var ipAddr = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
+port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+app.set('port', port);
 
-        if (typeof self.ipaddress === "undefined") {
-            console.warn('No OPENSHIFT_NODEJS_IP var, using 127.0.0.1');
-            self.ipaddress = "127.0.0.1";
-        };
-    };
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
+app.get('/test', function(req, res) {
+	res.send('Everything is Fine');
+});
 
-    self.terminator = function(sig){
-        if (typeof sig === "string") {
-            console.log('%s: Received %s - terminating sample app ...',
-                Date(Date.now()), sig);
-            process.exit(1);
-        }
-        console.log('%s: Node server stopped.', Date(Date.now()) );
-    };
+app.get('/',function(req,res){
+	res.send('Hello World!!!');
+})
+app.use(function(req, res, next) {
+	res.send({
+		msg: 'Invalid'
+	});
+});
 
-    self.setupTerminationHandlers = function(){
-        process.on('exit', function() { self.terminator(); });
-        ['SIGHUP', 'SIGINT', 'SIGQUIT', 'SIGILL', 'SIGTRAP', 'SIGABRT',
-            'SIGBUS', 'SIGFPE', 'SIGUSR1', 'SIGSEGV', 'SIGUSR2', 'SIGTERM'
-        ].forEach(function(element, index, array) {
-                process.on(element, function() { self.terminator(element); });
-            });
-    };
+http.createServer(app).listen(port, ipAddr, function() {
+	console.log("Server is listening at " + port);
+});
 
-    self.initializeServer = function() {
-        var app=self.app=express();
-        app.use(bodyParser.json());
-        app.use(bodyParser.urlencoded({extended:true}));
-        app.engine('html',require('ejs').renderFile);
+process.on('exit', function() {
+	console.log("Server is stoped " + new Date());
+});
 
-        app.set('view engine','html');
-        app.set('views',path.join(__dirname,'views'));
-        app.get('/',function(req,res){
-           res.render('index');
-        });
-        app.get('/test',function(req,res){
-            res.send('Working !!!');
-        });
-    };
-
-    self.initialize = function() {
-        self.setupVariables();
-        self.setupTerminationHandlers();
-        self.initializeServer();
-    };
-
-    self.start = function() {
-        //  Start the app on the specific interface (and port).
-        http.createServer(self.app).listen(self.port, self.ipaddress, function() {
-            console.log('%s: Node server started on %s:%d ...',
-                Date(Date.now() ), self.ipaddress, self.port);
-        });
-    };
-
-};
-
-var zapp = new SampleApp();
-zapp.initialize();
-zapp.start();
-
+['SIGHUP', 'SIGINT', 'SIGQUIT', 'SIGILL', 'SIGTRAP', 'SIGABRT',
+	'SIGBUS', 'SIGFPE', 'SIGUSR1', 'SIGSEGV', 'SIGUSR2', 'SIGTERM'
+].forEach(function(element, index, array) {
+	process.on(element, function() {
+		process.exit(1);
+	});
+});
